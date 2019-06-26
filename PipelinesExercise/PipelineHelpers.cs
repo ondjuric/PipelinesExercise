deae24d4e8071f2b151c40df9cb6104511d33058
Llewellyn Falco <llewellyn.falco@gmail.com>
2019-06-25 20:19:35 -0700

    F Helpers?

---
+++ PipelinesExercise/PipelineHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Refactoring.Pipelines;
+
+namespace PipelinesExercise
+{
+    public static class PipelineHelpers
+    {
+        public static FunctionPipe<IEnumerable<TInput>, List<TOutput>> ProcessForEach<TInput, TOutput>(
+            this Sender<IEnumerable<TInput>> sender, Func<TInput, TOutput> func)
+        {
+            return new FunctionPipe<IEnumerable<TInput>, List<TOutput>>(p => p.Select(func).ToList(), sender);
+        }
+    }
+}
