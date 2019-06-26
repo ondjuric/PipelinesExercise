6b2a3f05cfe85528a925d6354b8c7e9e03283ff4
Llewellyn Falco <llewellyn.falco@gmail.com>
2019-06-25 20:29:05 -0700

    F working Dot reporter

---
+++ PipelinesExercise.Tests/WorkingDotReporter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using ApprovalTests.Reporters;
+using Refactoring.Pipelines.Approvals;
+
+namespace Tests
+{
+    public class WorkingDotReporter : GenericDiffReporter
+        {
+            public static readonly WorkingDotReporter INSTANCE = new WorkingDotReporter();
+
+            public WorkingDotReporter()
+                : base(new DiffInfo(@"{ProgramFiles}\Microsoft VS Code\Code.exe", "-r {0}", (Func<IEnumerable<string>>)(() => (IEnumerable<string>)new string[1]
+                {
+                    "dot"
+                })))
+            {
+            }
+        }
+}
